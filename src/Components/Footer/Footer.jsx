27d37300386e75fa5d7b62ef83393579f24505a9
omkar902712 <omkarp9027@gmail.com>
2026-04-28 15:15:28 +0530

    [Footer.jsx - Changes And Proper Align]

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,42 +2,133 @@ import React from 'react';
 import './Footer.css';
 import { Link } from 'react-router-dom';
 
+// import images
+import footer_fb from '../../assets/Images/Footer/footer_fb.jpg';
+import footer_twitter from '../../assets/Images/Footer/footer_twitter.png';
+import footer_insta from '../../assets/Images/Footer/footer_insta.jpg';
+
 const Footer = () => {
   return (
     <footer className="site-footer">
       <div className="footer-container">
-        <div className="footer-column about">
-          <h3> Social Media App </h3>
-          <p>Building modern web experiences with clean code and beautiful design.</p>
-        </div>
-
-        <div className="footer-column links">
-          <h4>Quick Links</h4>
-          <ul>
-            <li>
-              <Link to="/home" className="nav-link">
-                Home
-              </Link>
-              <Link to="/profile" className="nav-link">
-                Profile
-              </Link>
-              <Link to="" className="nav-link">
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="footer-column newsletter">
-          <h4>Stay Updated</h4>
-          <form className="row">
-            <div className="col-12">
-              <input type="email" placeholder="Enter your email" required />
-              <button type="submit">Subscribe</button>
+        {/* Social Media App And Logo */}
+        <div className='row'>
+          <div className='col-sm-4'>
+            <div className="footer-column">
+              <h3> Social Media App </h3>
+              <p>Building modern web experiences with clean code and beautiful design.</p>
             </div>
-          </form>
+          </div>
+
+          <div className='col-sm-5'> </div>
+
+          <div className='col-sm-3'>
+            <Link>
+              <img src={footer_fb} alt="Facebook Logo" className='footer_logo' />
+            </Link>
+
+            <Link>
+              <img src={footer_insta} alt="Insta Logo" className='footer_logo' />
+            </Link>
+
+            <Link>
+              <img src={footer_twitter} alt="Twitter Logo" className='footer_logo' />
+            </Link>
+          </div>
+        </div>        
+
+        {/* Resources, Pricing, Comapny, Social */}
+        <div className='row'>
+
+          <div className='col-sm-3'>
+            <div className="footer-column links">
+              <h4> Resources </h4>
+              <ul>
+                <li>
+                  <Link to="" className="nav-link">
+                    Application
+                  </Link>
+
+                  <Link to="" className="nav-link">
+                    Documentation
+                  </Link>
+
+                  <Link to="" className="nav-link"> Sytem </Link>
+
+                  <Link to="" className="nav-link"> FAQ </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='col-sm-3'>
+            <div className="footer-column links">
+              <h4> Pricing </h4>
+              <ul>
+                <li>
+                  <Link to="" className="nav-link">
+                    Overwiew
+                  </Link>
+                  <Link to="" className="nav-link">
+                    Premium Plans
+                  </Link>
+                  <Link to="" className="nav-link">
+                    Affilate Program
+                  </Link>
+                  <Link to="" className="nav-link">
+                    Promotions  
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='col-sm-3'>
+            <div className="footer-column links">
+              <h4>Comapny</h4>
+              <ul>
+                <li>
+                  <Link to="" className="nav-link">
+                    About Us
+                  </Link>
+                  <Link to="" className="nav-link">
+                    Blog
+                  </Link>
+                  <Link to="" className="nav-link">
+                    Partnrship
+                  </Link>                  
+                  <Link to="" className="nav-link">
+                    Careers
+                  </Link>
+                  <Link to="" className="nav-link">
+                    Press
+                  </Link>                
+                                  
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='col-sm-3'>
+            <div className="footer-column links">
+              <h4>Social</h4>
+              <ul>
+                <li>
+                  <Link to="" className="nav-link">
+                    Facebook
+                  </Link>
+                  <Link to="" className="nav-link">
+                    Instragram
+                  </Link>
+                  <Link to="" className="nav-link">
+                    Twitter
+                  </Link>                  
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
+      </div>        
 
       <div className="footer-bottom">
         <p>&copy; 2026 Social Media App. All rights reserved.</p>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './Context/AuthContext.jsx'
 
+// import Boostrap 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
